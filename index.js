fe80a3b7210b127262ceee4e3262b596b096e55e
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-01-10 20:23:49 +0100

    Add javascript for the New In section
    Create the effect of showing description when clicking on image by using "onclick" and "data" attribute and Classlist Toggle

---
+++ index.js
@@ -0,0 +1,22 @@
+const description = document.querySelectorAll('.new-in-description')
+// Create an array of all elements with the class name
+function myFunction(event) {
+        const picIndex = event.srcElement.dataset.pic;
+        // choose a data from the srcElement in the event from Console as the identifier for this logic
+        // console.log(event.srcElement);
+        description[+picIndex].classList.toggle('new-in-description-showed')
+    }
+        // + in front to change string into numbers
+    
+    
+    
+    
+    // for(let x of description) {
+        // x.classList.toggle('new-in-description-showed')} >> it works on all images at once
+    
+    /* // data-pic="0"
+    const productImg = document.querySelectorAll('.product_image img')
+    productImg.forEach ((product,i) => {
+        product.dataset.pic= i.toString();
+    })
+     */
